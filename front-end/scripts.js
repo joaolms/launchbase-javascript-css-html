582527394966a5d55fa983378e4e6cbb56441ba5
João Sobrinho <joao.sobrinho@shift.com.br>
2020-04-20 19:05:03 -0300

    Final dos tópicos: Introdução à programação Web e Iniciando no Front-end.

---
+++ front-end/scripts.js
@@ -0,0 +1,22 @@
+// Selecionando a dive com a classe modal-overlay
+const modalOverlay = document.querySelector('.modal-overlay');
+
+// Selecionando a div card que possu
+const cards = document.querySelectorAll('.card');
+
+// Um for para ativar o modal quando clicar o card, verificar o ID do card que é o mesmo
+// ID do vídeo do Youtube e colocá-lo no link, por isso tem um loop, pois cada card tem sem
+// prório ID.
+for (let card of cards) {
+    card.addEventListener("click", function() {
+        const videoId = card.getAttribute("id");
+        modalOverlay.classList.add('active');
+        modalOverlay.querySelector("iframe").src = `https://www.youtube.com/embed/${videoId}`;
+    })
+};
+
+// Fechar o modal
+document.querySelector(".close-modal").addEventListener("click", function() {
+    modalOverlay.classList.remove("active");
+    modalOverlay.querySelector("iframe").src = "";
+});
